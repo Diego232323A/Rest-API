ba3d2f0a227b258fc1416a97200212ca87f4e502
Diego232323A <dachicaizaq@uce.edu.ec>
2025-01-13 23:27:08 -0500

    updating data

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Ruta raíz que devuelve un mensaje de "Hola Mundo"
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return "Hola Mundo desde una REST API con Flask"
+    # return jsonify({"message": "Hola Mundo desde una REST API con Flask"})
+
+# Ejecutar la aplicación en modo de depuración
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
