25503c5f638ee2dcd764abc410ba5a6b73b3c5fb
Diego232323A <dachicaizaq@uce.edu.ec>
2025-01-16 12:33:00 -0500

    updating

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello_world():
 
 # Ejecutar la aplicación en modo de depuración
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=80)
